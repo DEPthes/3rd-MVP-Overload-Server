2802827afb7ae511a535926c436901f1d6c08901
강승우 <phonil@naver.com>
2024-07-12 14:52:24 +0900

    [SETTING]: init project

---
+++ src/main/java/mvp/deplog/DePlogApplication.java
@@ -0,0 +1,13 @@
+package mvp.deplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DePlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DePlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mvp/deplog/DePlogApplicationTests.java
@@ -0,0 +1,13 @@
+package mvp.deplog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DePlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
