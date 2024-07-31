7b18e78e2c7150088d464e43652c9ccb3ee7fbb2
강승우 <phonil@naver.com>
2024-08-01 01:53:01 +0900

    [FEAT]: jsoup, commonmark 의존성 추가 / 게시글 작성 로직 변경

--- src/main/java/mvp/deplog/domain/post/application/PostService.java
+++ src/main/java/mvp/deplog/domain/post/application/PostService.java
@@ -10,6 +10,8 @@ import mvp.deplog.domain.tag.domain.Tag;
 import mvp.deplog.domain.tag.domain.repository.TagRepository;
 import mvp.deplog.domain.tagging.Tagging;
 import mvp.deplog.domain.tagging.repository.TaggingRepository;
+import mvp.deplog.global.common.SuccessResponse;
+import mvp.deplog.infrastructure.MarkdownUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @Service
 public class PostService {
+
     private final PostRepository postRepository;
     private final TagRepository tagRepository;
     private final TaggingRepository taggingRepository;
@@ -25,24 +28,31 @@ public class PostService {
     @Transactional
     public ResponseEntity<String> createPost(PostReq postReq, Member member) {
 
+        String content = postReq.getContent();
+        String previewContent = MarkdownUtil.extractPreviewContent(content);
+        String previewImage = MarkdownUtil.extractPreviewImage(content);
+
         Post post = Post.builder()
-                        .member(member)
-                        .title(postReq.getTitle())
-                        .content(postReq.getContent())
-                        .stage(Stage.PUBLISHED)
-                        .build();
+                .member(member)
+                .title(postReq.getTitle())
+                .content(content)
+                .previewContent(previewContent)
+                .previewImage(previewImage)
+                .stage(Stage.PUBLISHED)
+                .build();
         // 게시글 저장
         postRepository.save(post);
 
         // 태그 저장 & Tagging 엔티티 연결
         for(String tagName : postReq.getTagNameList()) {
             Tag tag = tagRepository.findByName(tagName)
-                    .orElseGet(() -> tagRepository.save(Tag.builder().name(tagName).build()));
+                    .orElseGet(() ->
+                            tagRepository.save(Tag.builder().name(tagName).build()));
 
             Tagging tagging = Tagging.builder()
-                                    .post(post)
-                                    .tag(tag)
-                                    .build();
+                    .post(post)
+                    .tag(tag)
+                    .build();
             taggingRepository.save(tagging);
         }
 
---
+++ src/main/java/mvp/deplog/infrastructure/MarkdownUtil.java
@@ -0,0 +1,68 @@
+package mvp.deplog.infrastructure;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarkdownUtil {
+
+    private static final String EMPTY_STRING = "";
+
+    // Markdown -> (Html) -> Text
+    public static String extractPlainText(String markdown) {
+        String html = markdownToHtml(markdown);
+        String text = Jsoup.parse(html).text();
+
+        return text.isEmpty() ? EMPTY_STRING : text;
+    }
+
+    // Markdown -> (Html) -> ImageUrl list
+    public static List<String> extractImageLinks(String markdown) {
+        String html = markdownToHtml(markdown);
+
+        Document document = Jsoup.parse(html);
+        Elements imgElements = document.select("img");
+        List<String> imageLinks = new ArrayList<>();
+        for (Element imgElement : imgElements) {
+            imageLinks.add(imgElement.attr("src"));
+        }
+        return imageLinks;
+    }
+
+    // Markdown -> (Html) -> Text -> Preview Content
+    public static String extractPreviewContent(String content) {
+        if (content == null || content.isEmpty())
+            return EMPTY_STRING;
+
+        String plainText = extractPlainText(content);
+        if (plainText.length() > 100)
+            return plainText.substring(0, 100);
+         else
+             return plainText;
+    }
+
+    // Markdown -> (Html) -> First ImageUrl
+    public static String extractPreviewImage(String markdown) {
+        String html = markdownToHtml(markdown);
+
+        Document document = Jsoup.parse(html);
+        Elements imgElements = document.select("img");
+
+        return imgElements.isEmpty() ? EMPTY_STRING : imgElements.get(0).attr("src");
+    }
+
+    // Markdown -> Html
+    public static String markdownToHtml(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+}
