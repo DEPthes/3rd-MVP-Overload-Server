d781ba3cef019b2f6205c8f24842fcd2c333aad1
강승우 <phonil@naver.com>
2024-08-12 01:34:40 +0900

    [REFACTOR]: Redis 설정 주석 처리

--- src/main/java/mvp/deplog/infrastructure/redis/RedisConfig.java
+++ src/main/java/mvp/deplog/infrastructure/redis/RedisConfig.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -21,6 +23,9 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+//        config.setPassword(redisPassword);
+//        return new LettuceConnectionFactory(config);
         return new LettuceConnectionFactory(host, port);
     }
 
@@ -29,5 +34,21 @@ public class RedisConfig {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory((redisConnectionFactory()));
         return redisTemplate;
+
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory());
+//
+//        // 일반적인 key:value의 경우 시리얼라이저
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setValueSerializer(new StringRedisSerializer());
+//
+//        // Hash를 사용할 경우 시리얼라이저
+//        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+//
+//        // 모든 경우
+//        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+//
+//        return redisTemplate;
     }
 }
