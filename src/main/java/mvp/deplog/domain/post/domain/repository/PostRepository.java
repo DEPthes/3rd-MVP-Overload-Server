4ec5b06eab514e9e40e739a6e8daf20f32ee65a5
HuniHuns <i690367@gmail.com>
2024-08-12 18:33:22 +0900

    [REFACTOR]: 임시 저장 게시글 조회 제외

--- src/main/java/mvp/deplog/domain/post/domain/repository/PostRepository.java
+++ src/main/java/mvp/deplog/domain/post/domain/repository/PostRepository.java
@@ -1,19 +1,29 @@
 package mvp.deplog.domain.post.domain.repository;
 
+import jakarta.persistence.QueryHint;
 import mvp.deplog.domain.member.domain.Part;
 import mvp.deplog.domain.post.domain.Post;
+import mvp.deplog.domain.post.domain.Stage;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query("SELECT p FROM Post p WHERE p.member.part IN :parts")
+    Page<Post> findAllByStage(Stage stage, Pageable pageable);
+
+    @Query("SELECT p FROM Post p WHERE p.member.part IN :parts AND p.stage = 'PUBLISHED'")
     Page<Post> findByMemberPart(@Param("parts") List<Part> partGroup, Pageable pageable);
 
-    Page<Post> findByTitleContainingOrContentContaining(String titleSearchWord, String contentSearchWord, Pageable pageable);
+    @Query("SELECT p FROM Post p WHERE (p.title LIKE %:titleSearchWord% OR p.content LIKE %:contentSearchWord%) " +
+            "AND p.stage = 'PUBLISHED'")
+    @QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "false"))
+    Page<Post> findByTitleContainingOrContentContaining(@Param("titleSearchWord") String titleSearchWord,
+                                                        @Param("contentSearchWord") String contentSearchWord,
+                                                        Pageable pageable);
 }
