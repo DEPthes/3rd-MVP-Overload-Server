03ffe266758cbd2c0e1450cdb03305e3150843cf
HuniHuns <i690367@gmail.com>
2024-08-18 03:42:21 +0900

    [REFACTOR]: 미사용 메소드 삭제

--- src/main/java/mvp/deplog/domain/comment/application/CreateMemberCommentServiceImpl.java
+++ src/main/java/mvp/deplog/domain/comment/application/CreateMemberCommentServiceImpl.java
@@ -56,12 +56,4 @@ public class CreateMemberCommentServiceImpl implements CreateCommentService {
 
         return SuccessResponse.of(message);
     }
-
-    private String commentNickname(Comment comment){
-        if(!comment.isUseNickname()) {
-            Member member = comment.getMember();
-            return member.getName();
-        }
-        return comment.getNickname();
-    }
 }
