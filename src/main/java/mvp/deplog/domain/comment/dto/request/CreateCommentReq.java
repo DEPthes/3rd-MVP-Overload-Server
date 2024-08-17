1386c02c599a739ee2a250c3494f38f987eaacb9
HuniHuns <i690367@gmail.com>
2024-08-18 00:22:06 +0900

    [REFACTOR]: 닉네임, 내용 NotBlank 설정

--- src/main/java/mvp/deplog/domain/comment/dto/request/CreateCommentReq.java
+++ src/main/java/mvp/deplog/domain/comment/dto/request/CreateCommentReq.java
@@ -1,6 +1,7 @@
 package mvp.deplog.domain.comment.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -13,9 +14,11 @@ public class CreateCommentReq {
     private Long postId;
 
     @Schema(type = "String", example = "**댓글 내용**", description = "댓글 내용입니다.")
+    @NotBlank
     private String content;
 
     @Schema(type = "String", example = "닉네임", description = "닉네임입니다.")
+    @NotBlank
     private String nickname;
 
     @Schema(type = "boolean", example = "true", description = "닉네임 사용 체크박스 체크 여부입니다. 회원이 체크 안한 경우, 비회원의 경우 모두 true입니다. 회원이 누른 경우만 false입니다.")
